5cf25f6d4d786f6a8760131c53e5515d74d55597
ayaelas <assalharras@gmail.com>
2024-07-15 00:54:09 +0200

    fun

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,52 @@
+#!/usr/bin/node
+
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api';
+const movieId = process.argv[2];
+const movieUrl = `${API_URL}/films/${movieId}/`;
+
+// Check if the movie ID is provided
+if (!movieId) {
+  console.error('Please provide a Movie ID as the first positional argument');
+  process.exit(1);
+}
+request(movieUrl, (err, _, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+
+  // Parse the JSON response body
+  const filmData = JSON.parse(body);
+
+  // Extract character URLs from the film data
+  const characterUrls = filmData.characters;
+
+  // Function to fetch character names asynchronously
+  const fetchCharacterNames = () => {
+    const promises = characterUrls.map(url => {
+      return new Promise((resolve, reject) => {
+        request(url, (err, _, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        });
+      });
+    });
+
+    // Resolve all promises and print character names
+    Promise.all(promises)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(error => {
+        console.error('Error fetching characters:', error);
+      });
+  };
+
+  // Execute function to fetch and print character names
+  fetchCharacterNames();
+});
